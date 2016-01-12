2c3d9c8e997c06e34239d675eb984584a1f17b1f
Ben de Graaff <ben@nx3d.org>
2016-01-12 19:25:41 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='cryptfile',
+      version='1.0',
+      description='Mounting LUKS files made easy',
+      author='Ben de Graaff',
+      author_email='ben@nx3d.org',
+      url='https://github.com/Bun/cryptfile',
+      scripts=['cryptfile'])
